acc446fba9170ff84fd4e82ac718913a551ef6db
Eduardo <eduardobarrossferreira@gmail.com>
2026-03-25 15:02:40 -0300

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -26,21 +26,21 @@ fun fazerLogininicial (){
         }
     }
     if (acessoconcedido) {
-        val hotel = "HOTEL EDU PALACE"
+        print("Digite o seu nome?")
         val nome = readln()
-        println("Bem-vindo ao $hotel! É um imenso prazer ter você por aqui!")
+        println("Bem-vindo $nome! É um imenso prazer ter você por aqui!")
     } else {
         println("Sistema bloqueado por excesso de tentativas.")
-        inicio()
     }
+    Menuprincipal()
 }
 
 // Menu principal
-fun inicio (){
+fun  Menuprincipal (){
     var opcao: Int
    do {
      println("\n --- Menu Principal--- ")
-      println("1 - Reserva de quartos ")
+      println("1 - Reservadequartos ")
      println("2 - Cadatro de hóspedes ")
      println("3 - Eventos ")
      println("4 - Ar - Condicionado ")
@@ -48,24 +48,46 @@ fun inicio (){
      println("6 - Relatórios operacionais ")
      print ("0 - sair")
 
-     opcao = readln()?.toIntOrNull() ?: -1
+       println("\n Digite sua opção: ")
 
+     opcao = readln()?.toIntOrNull() ?: -1
        when (opcao) {
-           1 -> println("Você escolheu cadastrar usuário.")
-           2 -> println("Você escolheu listar usuários.")
-           3 -> println("Você escolheu excluir usuário.")
+        1 -> println("Você escolheu reservar quartos")
+           2 ->  println("Você escolheu cadastrar hóspedes")
+           3 -> println("Você escolheu eventos")
+               4-> println("Você escolheu ar-condicionado")
+                   5 -> println("Você escolheu abastecer")
+                       6 -> println("Você escolheu ver os relatórios operacionais")
            0 -> println("Saindo do sistema...")
            else -> println("Opção inválida, tente novamente.")
        }
        println()
    } while (opcao != 0)
+   Reservadequartos()
 }
 
+     // Reserva de quartos (Subprograma)
+     fun  Reservadequartos() {
+         println("--- [Reservas] --- ")
+print ("Informe o valor da diária: ")
+         val Valordadiária = readln().toDouble() ?: 0.0
+         print ("Informe a quantidade de diárias (1-20) : ")
+         val dias = readln().toIntOrNull() ?:0
 
+         if (Valordadiária <= 0 || dias !in 1..30)
+         println("Valor inválido")
+        return
 
+     }
 
-
-
+    print ("Informe o nome do hóspede:")
+        val nomeH = readln
+print ("Tipo de quarto: S- suíte| D- deluxe| E- Executiva: ")
+        val fator = when (readln().uppercase()){
+            "s"
+            "L"
+            else 
+        }
 
 
 
